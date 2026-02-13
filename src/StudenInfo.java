3c9b7eebeca7f81541861f9181d0c9065054dcf4
0younge <s45628439@gmail.com>
2026-02-13 14:32:25 +0900

    내일배움캠프 사전캠프 변수와 자료형 미션

---
+++ src/StudenInfo.java
@@ -0,0 +1,20 @@
+public class StudenInfo {
+    public static void main(String[] args) {
+
+        // TODO : 학생 정보 변수 선언과 초기화
+        int age = 23;
+        double height = 168.0;
+        char grade = '2';
+        boolean isStudent = false;
+        String name = "권영현";
+
+        // TODO : 학생 정보 출력
+        System.out.println("=== 학생 정보 ===");
+        System.out.println("이름: " + name );
+        System.out.println("나이: " + age );
+        System.out.println("키: " + height );
+        System.out.println("학년: " + grade );
+        System.out.println("재학중: " + isStudent );
+
+    }
+}
